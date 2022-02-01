887b358a5b906d1b583dec8da8cb7e9ab847a2d1
joscord <josecorderodev@gmail.com>
2022-02-01 11:12:09 -0300

    Modified invertedStr and palindromeChecker functions

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,10 +1,13 @@
+// regex for whitespace
+const regex = /\s+/g
+
 // Inverted string function
 const invertedStr = str => {
         // string to array 
         const strArray = str.split('');
         // reverse the array
         const invertedArray = strArray.reverse();
-        // Array to string
+        // Array to string/ 
         return invertedArray.join('');
 }
 
@@ -12,11 +15,14 @@ const invertedStr = str => {
 const palindromeChecker = str => {
     if (typeof str !== 'string') return 'Please enter a valid string'
     const invertedString = invertedStr(str.toLowerCase());
-    if (str === invertedString) return "It's a Palindrome"
-    else "It's not a Palindrome"
+    console.log(str.replace(regex, ''));
+    console.log(invertedString.replace(regex, ''));
+    if (str.replace(regex, '') === invertedString.replace(regex, '')) return "It's a Palindrome"
+    else return "It's not a Palindrome"
 };
 
-console.log(palindromeChecker('ana'));
-console.log(palindromeChecker('Ana'));
-console.log(palindromeChecker('Banana'));
+// console.log(palindromeChecker('ana'));
+// console.log(palindromeChecker('Ana'));
+// console.log(palindromeChecker('banana'));
+console.log(palindromeChecker('luz azul'))
 console.log(palindromeChecker());
