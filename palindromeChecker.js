7b215183f33058362c145f6b2f44e113e5723ea8
joscord <josecorderodev@gmail.com>
2022-02-01 11:18:31 -0300

    Modified palindromeChecker function

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -14,15 +14,16 @@ const invertedStr = str => {
 // palindromeChecker
 const palindromeChecker = str => {
     if (typeof str !== 'string') return 'Please enter a valid string'
-    const invertedString = invertedStr(str.toLowerCase());
-    console.log(str.replace(regex, ''));
-    console.log(invertedString.replace(regex, ''));
-    if (str.replace(regex, '') === invertedString.replace(regex, '')) return "It's a Palindrome"
+    const invertedString = invertedStr(str).toLowerCase();
+    const normalString = str.toLowerCase();
+    console.log(invertedString);
+    console.log(normalString);
+    if (normalString.replace(regex, '') === invertedString.replace(regex, '')) return "It's a Palindrome"
     else return "It's not a Palindrome"
 };
 
-// console.log(palindromeChecker('ana'));
-// console.log(palindromeChecker('Ana'));
-// console.log(palindromeChecker('banana'));
+console.log(palindromeChecker('ana'));
+console.log(palindromeChecker('Ana'));
+console.log(palindromeChecker('banana'));
 console.log(palindromeChecker('luz azul'))
 console.log(palindromeChecker());
